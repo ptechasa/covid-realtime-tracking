a7a6c8d767dc5f45abbc79521bf3c2c148c6bbdc
miko <ptechasa@mail.ccsf.edu>
2020-03-20 20:47:52 -0700

    fetching data each country

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,13 +3,14 @@ var total_deaths = document.getElementById('total_deaths');
 var total_recovered = document.getElementById('total_recovered');
 var new_cases = document.getElementById('new_cases');
 var new_deaths = document.getElementById('new_deaths');
+var active_cases = document.getElementById('active_cases')
 var table = document.getElementById('countries');
 
 var apihost = config.HOST;
 var mykey = config.MY_KEY;
 
 
-//Fetching Data Total cases
+//Fetching all data around the world
 fetch("https://coronavirus-monitor.p.rapidapi.com/coronavirus/worldstat.php", {
 	"method": "GET",
 	"headers": {
@@ -24,8 +25,44 @@ fetch("https://coronavirus-monitor.p.rapidapi.com/coronavirus/worldstat.php", {
     total_recovered.innerHTML = data.total_recovered;
     new_cases.innerHTML = data.new_cases;
     new_deaths.innerHTML = data.new_deaths;
+    active_cases.innerHTML = data.active_cases;
 }))
 .catch(err => {
 	console.log(err);
 });
 
+
+//Fetching data by country
+fetch("https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": apihost,
+		"x-rapidapi-key": mykey
+	}
+})
+.then(response => response.json().then(data => {
+    console.log(data);
+    let countries = data.countries_stat;
+    console.log(countries);
+    for (let i = 0; i < countries.length; i ++) {
+        console.log(countries[i]);
+
+        let row = table.insertRow(i+1);
+        let country_name = row.insertCell(0);
+        let cases = row.insertCell(1);
+        let deaths = row.insertCell(2);
+        let serious_critical = row.insertCell(3);
+        let active_cases = row.insertCell(4);
+        let recovered = row.insertCell(5);
+
+        country_name.innerHTML = countries[i].country_name;
+        cases.innerHTML = countries[i].cases;
+        deaths.innerHTML = countries[i].deaths;
+        serious_critical.innerHTML = countries[i].serious_critical;
+        active_cases.innerHTML = countries[i].active_cases;
+        recovered.innerHTML = countries[i].total_recovered;
+    }
+}))
+.catch(err => {
+	console.log(err);
+});
